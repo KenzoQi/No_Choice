971b6c4fa4151db94517ec79c9ccf6ee14cbf44d
KenzoQi <pejana.jhonquincy18@gmail.com>
2024-05-05 08:19:11 +0800

    Updated
    Todo

---
+++ todos/resources/views/update.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.main')
+@push('head')
+    <title>Update Task</title>
+@endpush
+
+@section('main-section')
+    
+<div class=" container">
+    <div class="d-flex justify-content-between align-items-center my-5">
+        <div class="h2">Update Task</div>
+        <a href="{{route("todo.home")}}" class="btn btn-primary btn-lg">Back</a>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <form action="{{route("todo.updateData")}}" method="post">
+                @csrf
+                <label for="" class="form-label mt-4">Name</label>
+                <input type="text"name="name" class="form-control" value=""{{$todo->name}}>
+
+                <label for="" class="form-label mt-4">Work</label>
+                <input type="text"work="work" class="form-control" value=""{{$todo->work}}> 
+
+                <label for="" class="form-label mt-4">Due Date</label>
+                <input type="date"name="dueDate" class="form-control"value=""{{$todo->dueDate}}>
+
+                <input type="number" name="id" value="{{$todo->id}}" hidden>
+
+                <button class="btn btn-primary btn-lg mt-4">Update Task</button>
+            </form> 
+        </div>
+    </div>
+   
+ 
+</div>
+@endsection
